0d0d5d8cba3a0cee7545c5cf3af825ed3c259682
Tânia Esteves <tania_esteves@outlook.com>
2025-06-13 17:35:24 +0100

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,21 +14,24 @@ const config = {
   tagline: 'Description',
   favicon: 'img/favicon.ico',
 
+  trailingSlash: false, // Optional: cleaner URLs
+
+
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://taniaesteves.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tryout',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'taniaesteves', // Usually your GitHub org/user name.
+  projectName: 'XXX', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
